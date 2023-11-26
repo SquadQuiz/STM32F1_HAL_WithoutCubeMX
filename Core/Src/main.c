7d0d22be4b5282335a45e5bd6937e2b4ff55fedb
SquadQuiz <amornsakstp6@gmail.com>
2023-11-26 17:20:01 +0700

    Added: SPI1 with RC522 RFID Reader Application, Demo: Card ID Comparison

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,11 +43,35 @@ int main(void)
 		printf("Register[0x%02X] = 0x%02X\n", i, rc522_regRead8(i));
 		HAL_Delay(10);
 	}
+	//RFID Card
+	uint8_t cardId[5];
+	uint8_t blueTagId[5] = { 0x33,0xEF,0xD3,0x99,0x96 };
+	rc522_init();
+	printf("Place your card\n");
 
 	while (1)
 	{
-		
-		gpio_LED_toggleGreen();
+		if (rc522_checkCard(cardId)) 
+		{
+			printf("Card is Detectied with ID:\n");
+			for (uint8_t i = 0; i < 5; i++) 
+			{
+				printf("[%d]: 0x%02X\n", i, cardId[i]);
+			}
+			if (rc522_compareIds(cardId, blueTagId)) 
+			{
+				printf("Success, access granted!\n");
+				gpio_LED_writeGreen(1);
+			}
+			else
+			{
+				printf("Failed, access denied!\n");
+				gpio_LED_writeGreen(0);
+			}
+			printf("******************\n\n");
+			HAL_Delay(2000);
+			gpio_LED_writeGreen(0);
+		}
 		HAL_Delay(500);
 	}
 }
