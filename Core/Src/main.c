c9fabb211a99494de6e8253c36de1dc2fdd31433
SquadQuiz <amornsakstp6@gmail.com>
2023-11-26 19:23:10 +0700

    Added I2C1 with MPU6050 IMU Sensor, Demo read accelerometer value from MPU6050

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -16,6 +16,7 @@
 #include "spi.h"
 #include "rc522.h"
 #include "i2c.h"
+#include "TJ_MPU6050.h"
 
 int main(void)
 {
@@ -35,6 +36,16 @@ int main(void)
 	//* I2C Peripheral
 	i2c_I2C1_gpio_config();
 	i2c_config();
+	//* MPU6050 Init
+	MPU_ConfigTypeDef mpuConfig;
+	mpuConfig.Accel_Full_Scale = AFS_SEL_4g;
+	mpuConfig.ClockSource = Internal_8MHz;
+	mpuConfig.CONFIG_DLPF = DLPF_184A_188G_Hz;
+	mpuConfig.Gyro_Full_Scale = FS_SEL_500;
+	mpuConfig.Sleep_Mode_Bit = 0;
+	MPU6050_Config(&mpuConfig);
+
+	ScaledData_Def accelScaleDef;
 
 	// Check I2C Slave Address.
 	if (HAL_I2C_IsDeviceReady(&i2c1Handle, (0x68 << 1), 5, HAL_MAX_DELAY) == HAL_OK)
@@ -47,8 +58,15 @@ int main(void)
 		printf("MPU6050 Device is not Found!\n");
 		gpio_LED_writeGreen(0);
 	}
+	
+
 	while (1)
 	{
+		MPU6050_Get_Accel_Scale(&accelScaleDef);
+		printf("Accel:\nX=%.2f \nY=%.2f \nZ=%.2f\n", accelScaleDef.x, accelScaleDef.y, accelScaleDef.z);
+		printf("************************\n\n");
+		gpio_LED_toggleGreen();
+		HAL_Delay(250);
 	}
 }
 
